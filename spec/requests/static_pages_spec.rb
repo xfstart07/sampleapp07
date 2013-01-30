a3acaac827e21270e5f0de93ec8728a960529680
YiFei Xu <xfstart07@gmail.com>
2013-01-30 15:44:49 +0800

    added contact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,11 +12,14 @@ describe "StaticPages" do
   #     response.status.should be(200)
   #   end
   # end
+  
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+   
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
   
@@ -24,7 +27,7 @@ describe "StaticPages" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
   
@@ -32,7 +35,15 @@ describe "StaticPages" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
+    end
+  end
+  
+  describe "Contact page" do 
+    it "should have the content 'Contact'" do 
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+      page.should have_selector('title', :text => "#{base_title} | Contact")
     end
   end
 end
