ea3c5d4ec1f2b198a8c36e9c6833fd3dbcd5de99
Xu Fei <xfstart07@gmail.com>
2013-04-16 19:59:07 +0800

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
-  
+  has_secure_password
+
   before_save { |user|
     user.email = email.downcase
   }
